fcc78e4907632f3142272b089377f5e49972f2be
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-03-06 11:26:03 +0100

    add response timer

--- app.js
+++ app.js
@@ -12,14 +12,23 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.use(
-  "/api",
-  graphqlHttp({
-    schema: schemaDefinition,
-    rootValue: resolvers,
-    graphiql: true
+app.use((req, res, next) => {
+    const start = Date.now();
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      console.log(`${duration} ms`);
+
+    });
+    next();
   })
-);
+  .use(
+    "/api",
+    graphqlHttp({
+      schema: schemaDefinition,
+      rootValue: resolvers,
+      graphiql: true
+    })
+  );
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-w90yh.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
   .then(() => {
