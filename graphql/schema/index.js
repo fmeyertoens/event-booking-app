4fd03c9ce1f13748534a2cb070a32db157d76cfd
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-03-12 16:42:00 +0100

    add jsonwebtoken generation
    generation in user.js

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -34,6 +34,12 @@ module.exports = buildSchema(`
     createdEvents: [Event!]
   }
 
+  type AuthData {
+    userId: ID!
+    token: String!
+    tokenExpiration: Int!
+  }
+
   input UserInput {
     email: String!
     password: String!
@@ -42,6 +48,7 @@ module.exports = buildSchema(`
   type RootQuery {
     events: [Event!]!
     bookings: [Booking!]!
+    login(email: String!, password: String!): AuthData!
   }
 
   type RootMutation {
