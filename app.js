5144b328d6b8d0f507e429ab2791badb34864e52
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-03-15 16:44:51 +0100

    add CORS headers
    respond to options request

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Max-Age', '600');
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use((req, res, next) => {
@@ -33,7 +44,7 @@ app.use((req, res, next) => {
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-w90yh.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
   .then(() => {
-    app.listen(3000);
+    app.listen(8000);
   }).catch(err => {
     console.log(err);
   })
