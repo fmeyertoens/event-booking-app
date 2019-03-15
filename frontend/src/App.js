4a60a940867fd7f88d46d2bfe36cdfd61683541f
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-03-16 00:04:40 +0100

    add login and signup
    navbar and routing changes based on login status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,45 @@ import AuthPage from './pages/auth';
 import EventsPage from './pages/events';
 import BookingsPage from './pages/bookings';
 import MainNavigation from './components/navigation/mainNavigation';
+import AuthContext from './context/auth-context';
 import './App.css';
 
 class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  }
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({token: token, userId: userId});
+  };
+
+  logout = () => {
+
+  };
+
   render() {
     return (
       <BrowserRouter>
-      <React.Fragment>
-      <MainNavigation />
-      <main className="main-content">
-        <Switch>
-          <Redirect from="/" to="/auth" exact/>
-          <Route path="/auth" component={AuthPage}/>
-          <Route path="/events" component={EventsPage}/>
-          <Route path="/bookings" component={BookingsPage}/>
-        </Switch>
-      </main>
-      </React.Fragment>
+        <React.Fragment>
+          <AuthContext.Provider value={{
+            token: this.state.token,
+            userId: this.state.userId,
+            login: this.login,
+            logout: this.logout}}>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              {!this.state.token && <Redirect from="/" to="/auth" exact/>}
+              {this.state.token && <Redirect from="/" to="/events" exact/>}
+              {this.state.token && <Redirect from="/auth" to="/events" exact/>}
+              {!this.state.token && <Route path="/auth" component={AuthPage}/>}
+              <Route path="/events" component={EventsPage}/>
+              {this.state.token && <Route path="/bookings" component={BookingsPage}/>}
+            </Switch>
+          </main>
+          </AuthContext.Provider>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -1,23 +1,89 @@
 import React, { Component } from 'react';
+import AuthContext from './../context/auth-context';
 import './auth.css';
 
 class AuthPage extends Component {
+  state = {
+    showLogin: true,
+  };
+
+  static contextType = AuthContext;
+
   constructor(props) {
     super(props);
     this.emailEl = React.createRef();
     this.passwordEl = React.createRef();
   }
 
-  submitHandler = () => {
+  switchMode = () => {
+    this.setState(prevState => {
+      return {showLogin: !prevState.showLogin};
+    });
+  };
+
+  submitHandler = async (event) => {
+    event.preventDefault();
     const email = this.emailEl.current.value;
     const password = this.passwordEl.current.value;
 
+    if(email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
 
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!this.state.showLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {
+              email: "${email}",
+              password: "${password}"
+            }) {
+              id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    try {
+      const response = await fetch('http://localhost:8000/api', {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Failed');
+      }
+      const resData = await response.json();
+      if(resData.data.login.token) {
+        this.context.login(
+          resData.data.login.token,
+          resData.data.login.token,
+          resData.data.login.token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
     return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={this.submitHandler}>
       <div className="form-control">
         <label htmlFor="email">E-Mail</label>
         <input type="email" id="email" ref={this.emailEl}/>
@@ -27,8 +93,8 @@ class AuthPage extends Component {
         <input type="password" id="password" ref={this.passwordEl}/>
       </div>
       <div className="form-actions">
-        <button type="button" className="secondary">Signup</button>
-        <button type="Submit">Login</button>
+        <button type="button" className="secondary" onClick={this.switchMode}>Switch to {this.state.showLogin ? 'Signup': 'Login'}</button>
+        <button type="Submit">{this.state.showLogin ? 'Login': 'Signup'}</button>
       </div>
     </form>);
   }
