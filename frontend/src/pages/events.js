ea382b9b99cb7b6f674fac82af9230d786ef6ecd
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-03-16 19:14:10 +0100

    add modal for creating events

--- frontend/src/pages/events.js
+++ frontend/src/pages/events.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import Modal from './../components/modal/modal';
+import Backdrop from './../components/backdrop/backdrop';
+import './events.css';
 
 class EventsPage extends Component {
+  state = {
+    modalIsOpen: false
+  };
+
+  openModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
+
   render() {
-    return <h1>The Events Page</h1>
+    return (
+      <React.Fragment>
+        {this.state.modalIsOpen && <Backdrop />}
+        {this.state.modalIsOpen && <Modal title="Add Event"
+        canCancel
+        canConfirm
+        onCancel={this.closeModal}
+        onConfirm={this.closeModal}>
+          <p>Modal Content</p>
+        </Modal>}
+        <div className="events-panel">
+          <p>Any events coming up?</p>
+          <button className="btn" onClick={this.openModal}>Create one</button>
+        </div>
+      </React.Fragment>
+      );
   }
 }
 
