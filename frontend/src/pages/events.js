ae8a6ef7813c9bf898e99786910d3ad41674b4f9
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-03-19 14:31:18 +0100

    add event list and event creation

--- frontend/src/pages/events.js
+++ frontend/src/pages/events.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import Modal from './../components/modal/modal';
 import Backdrop from './../components/backdrop/backdrop';
 import Datetime from 'react-datetime';
+import AuthContext from '../context/auth-context';
+import moment from 'moment';
 import './events.css';
 import 'react-datetime/css/react-datetime.css';
 
 class EventsPage extends Component {
   state = {
     modalIsOpen: false,
-    eventDate: null
+    eventDate: null,
+    events: []
   };
+
+  static contextType = AuthContext;
 
   constructor(props) {
     super(props);
@@ -19,7 +24,12 @@ class EventsPage extends Component {
   }
 
   updateEventDate = (newDate) => {
-    this.setState({eventDate: newDate.toDate()});
+    if ("string" === typeof newDate) {
+      newDate = moment(newDate);
+    }
+    if (newDate.isValid()) {
+      this.setState({eventDate: newDate.toDate()});
+    }
   }
 
   openModal = () => {
@@ -30,25 +40,115 @@ class EventsPage extends Component {
     this.setState({modalIsOpen: false});
   }
 
-  confirmModal = () => {
+  confirmModal = async () => {
     this.setState({modalIsOpen: false});
     const title = this.titleElRef.current.value;
     const price = +this.priceElRef.current.value;
-    const date = this.state.eventDate.toISOString();
+    const date = this.state.eventDate ? this.state.eventDate.toISOString() : this.state.eventDate;
     const description = this.descriptionElRef.current.value;
 
     if (title.trim().length === 0
-      || this.priceElRef.current.value.trim().length === 0
+      || price <= 0
       || !date
       || description.trim().length === 0) {
       return;
     }
 
-    const event = {title, price, date, description};
-    console.log(event);
+    const requestBody = {
+      query: `
+        mutation {
+          createEvent(eventInput: {
+            title: "${title}",
+            description: "${description}",
+            price: ${price},
+            date: "${date}"
+          }) {
+            id
+            title
+            price
+            description
+            price
+            date
+            creator {
+              id
+              email
+            }
+          }
+        }
+      `
+    };
+
+    const token = this.context.token;
+
+    try {
+      const response = await fetch('http://localhost:8000/api', {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Failed');
+      }
+      const resData = await response.json();
+      
+      this.fetchEvents();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchEvents();
+  }
+
+  async fetchEvents() {
+    const requestBody = {
+      query: `
+        query {
+          events {
+            id
+            title
+            price
+            description
+            price
+            date
+            creator {
+              id
+              email
+            }
+          }
+        }
+      `
+    };
+
+    try {
+      const response = await fetch('http://localhost:8000/api', {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Failed');
+      }
+      const resData = await response.json();
+      
+      const events = resData.data.events;
+      this.setState({events: events});
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
+    const eventList = this.state.events.map(event => {
+      return <li key= {event.id} className="event-list-item">{event.title}</li>
+    });
+
     return (
       <React.Fragment>
         {this.state.modalIsOpen && <Backdrop />}
@@ -76,10 +176,15 @@ class EventsPage extends Component {
             </div>
           </form>
         </Modal>}
+        {this.context.token && (
         <div className="events-panel">
           <p>Any events coming up?</p>
           <button className="btn" onClick={this.openModal}>Create one</button>
         </div>
+        )}
+        <ul className="event-list">
+          {eventList}
+        </ul>
       </React.Fragment>
       );
   }
